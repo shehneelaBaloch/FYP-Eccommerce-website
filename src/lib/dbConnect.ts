b478cae97fa75fa54cae6c7fb9f5625725ca88ef
shehneelaBaloch <balochshahneela378@gmail.com>
2026-06-08 13:15:17 +0500

    code updated

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error("⚠️ Please define MONGODB_URI in .env.local");
-}
-
 let cached = (global as any).mongoose;
 
 if (!cached) {
@@ -13,6 +7,12 @@ if (!cached) {
 }
 
 async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI as string;
+
+  if (!MONGODB_URI) {
+    throw new Error("⚠️ Please define MONGODB_URI in .env.local");
+  }
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
